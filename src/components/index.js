56f63b7fc43dc36d13e324195a6e8e73b14bce9a
Isisaurus <dianavitanyi@gmail.com>
2021-06-23 14:17:30 +0200

    Bug fix.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Navbar } from './navbar/Navbar';
-export { default as Products } from './products/Products';
+export { default as Products } from './products/products';
 export { default as Cart } from './Cart/Cart';
 export { default as Checkout } from './CheckoutForm/Checkout/Checkout';
