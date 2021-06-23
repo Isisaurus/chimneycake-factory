57dfdd839b0c096b2da7792ccb2013519df287e4
Isisaurus <dianavitanyi@gmail.com>
2021-06-23 10:38:56 +0200

    Setup for stripe payments.

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 const PaymentForm = () => {
   return <div>PaymentForm</div>;
