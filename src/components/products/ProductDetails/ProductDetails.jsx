a188271f222b2c068d4fc3cbd674394510115fab
Isisaurus <dianavitanyi@gmail.com>
2021-06-23 17:21:13 +0200

    Product overview page markup.

--- src/components/products/ProductDetails/ProductDetails.jsx
+++ src/components/products/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+import { commerce } from '../../../lib/commerce';
 
 const ProductDetails = () => {
   const { id } = useParams();
+
+  const [product, setProduct] = useState();
+
+  const getProductById = async (id) => {
+    const res = await commerce.products.retrieve(id);
+    console.log(res);
+    // name, description, categories
+    // img: media.source
+    setProduct(res);
+  };
+
+  useEffect(() => {
+    getProductById(id);
+  }, []);
+
+  if (!product) return 'Loading...';
+
   return (
     <>
-      <div style={{ margin: '100px 0' }}>Product Overview {id}</div>
+      <div style={{ margin: '100px 0' }}>
+        {product.name}, {id}, {product.media.source}, {product.description}
+      </div>
+      <div>
+        {product.categories.map((cat) => (
+          <Link to={`/shop/:${cat.slug}`}>{cat.name}</Link>
+        ))}
+      </div>
     </>
   );
 };
