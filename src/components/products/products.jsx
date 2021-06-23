4c2926ab3d3607673512b3ce15438c8f1422bd09
Isisaurus <dianavitanyi@gmail.com>
2021-06-23 14:24:25 +0200

    Bug fix.

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 
-import Product from './product/Product';
+import Product from './Product/Product';
 import useStyles from './styles';
 
 const Products = ({ products, onAddToCart }) => {
