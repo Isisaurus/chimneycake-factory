9c687fe1abc3563994ce0df96616ef23024f711f
Isisaurus <dianavitanyi@gmail.com>
2021-06-24 21:49:22 +0200

    Hero section styling.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { Typography, Button, Grid, Container } from '@material-ui/core';
 import CategoryCard from './CategoryCard/CategoryCard';
 import useStyles from './styles';
-import heroImg from './../../assets/hero.jpg';
+import logo from './../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 const Home = ({ categories }) => {
   const classes = useStyles();
@@ -34,7 +35,43 @@ const Home = ({ categories }) => {
       <br />
       <br /> */}
       <Container className={classes.hero}>
-        <img src={heroImg} alt="Chimney Cake" className={classes.hero_img} />
+        <Grid
+          container
+          className={classes.introGrid}
+          justify="flex-end"
+          alignItems="center"
+        >
+          <Grid item className={classes.introItem} xs={12} md={4}>
+            <Typography
+              variant="body1"
+              color="primary"
+              className={classes.intro}
+              component="div"
+            >
+              <img src={`${logo}`} alt="logo" className={classes.logo} />
+              <ul className={classes.list}>
+                <li>hungarian delicaties</li>
+                <li>dutch preferences</li>
+                <li>premium CCH cakes</li>
+                <li>kürtőskalács</li>
+                <li>vegan & gluten-free choices</li>
+                <li>weekend specials</li>
+                <li>beekeepers' honey</li>
+                <li>babyshower gifts</li>
+              </ul>
+              <Button
+                className={classes.button}
+                variant="contained"
+                color="primary"
+                disableElevation
+                component={Link}
+                to="/shop"
+              >
+                Shop Now
+              </Button>
+            </Typography>
+          </Grid>
+        </Grid>
       </Container>
       <Grid container spacing={4} className={classes.grid}>
         {categories.map((category) => (
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,15 +1,59 @@
 import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles(() => ({
+import heroImg from './../../assets/hero.jpg';
+export default makeStyles((theme) => ({
   hero: {
     padding: '0',
-    maxHeight: '60rem',
-    height: '60vh',
-    overflow: 'hidden',
+    position: 'relative',
+    height: '80vh',
+    maxHeight: '40rem',
+    background: `url(${heroImg}) no-repeat center`,
   },
-  hero_img: {
+  hero_img: {},
+  introGrid: {
+    position: 'absolute',
+    [theme.breakpoints.down('md')]: {
+      width: '100%',
+      top: '0',
+      right: '0',
+      bottom: '0',
+    },
+    [theme.breakpoints.up('md')]: {
+      top: '-2rem',
+      right: '2rem',
+      height: '110%',
+      bottom: '-2rem',
+    },
+  },
+  introItem: {
+    background: 'rgba(19,24,31, 0.8)',
+    padding: '3rem',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  intro: {
+    listStylePosition: 'outside',
+    lineHeight: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  logo: {
     width: '100%',
-    height: 'auto',
+    maxWidth: '18rem',
+    padding: '1rem',
+  },
+  list: {
+    marginTop: '2.5rem',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '14px',
+    },
+  },
+  button: {
+    padding: '.8rem 2.5rem',
+    marginTop: '2.5rem',
   },
   grid: {
     marginTop: '5rem',
