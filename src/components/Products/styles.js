2d0a3e99957c947fc6c485f81f5b6439df51ae29
Isisaurus <dianavitanyi@gmail.com>
2021-06-27 12:44:51 +0200

    File changes

---
+++ src/components/Products/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  content: {
+    marginTop: '2.5rem',
+    marginBottom: '2.5rem',
+  },
+  grid: {
+    alignItems: 'flex-start',
+  },
+}));
